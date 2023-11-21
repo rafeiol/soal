5207639e60d158854cb1fffb628d5d71933f4205
rafeiol <rafaelbamedi74@gmail.com>
2023-11-21 13:22:11 +0330

    commit

---
+++ soal.py
@@ -0,0 +1,74 @@
+# # a=int(input("num1= "))
+# # b= int(input("num2= "))
+# # c= int(input("num3= "))
+
+# # if (a>b) and (a>c):
+# #     print(a)
+# # elif (b>a) and (b>c):
+# #     print(b)
+# # else:
+# #     print(c)
+    
+# # tol=int(input("tol= "))   
+# # arz=int(input("arz= "))
+# # masahat= tol*arz
+# # print( masahat)
+# # mohit=(tol+arz)*2
+# # print(mohit)
+# a=input("num= ")
+# a.split(',')
+# b=[]
+# b.append(a)
+# for z in b:
+#     if z%2==0:
+#         print(z)
+#     else:
+#         # print(z)a
+# a=input('num= ')
+# b=a.split(',')
+# for d in b:
+#      if int(d)%2==0:
+#        print(d)
+
+# c= int(input("num1= "))
+# v = int(input("num2= "))
+# def sum(a,b):
+#     suma = a+b
+#     return suma
+# print(sum(c,v))
+
+
+
+
+
+# def sumss(r):
+#     a=0
+#     for i in r:
+#         a+=i
+     
+#     return a
+
+# sumss(list_)       
+# zz= 50+ sumss(list_)
+# print(zz)
+
+        
+        
+
+list_=[2,3,33,45,65,12]
+def zoj(k):
+    kam=[]
+    for i in k:
+        if i%2==0:
+            kam.append(i)
+            # print(i)
+    return  kam         
+            
+    
+
+
+print(zoj(list_))
+            
+            
+    
+    
